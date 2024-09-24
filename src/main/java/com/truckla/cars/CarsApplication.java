91dde4d24a8446df1a9e5b961aabbe8d97736c5f
Edvinas K <alphab31174@gmail.com>
2024-09-24 14:39:20 +0300

    commit

---
+++ src/main/java/com/truckla/cars/CarsApplication.java
@@ -0,0 +1,14 @@
+package com.truckla.cars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsApplication {
+
+	public static void main(String[] args) {
+		//System.out.println("Entering test");
+		SpringApplication.run(CarsApplication.class, args);
+	}
+
+}
